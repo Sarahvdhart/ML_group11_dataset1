6e061dd51a5c55d237dc4233cf0ff662855df201
laraverhoef123 <l.verhoef-1@student.tudelft.nl>
2026-03-03 15:36:32 +0100

    ook korte versie + pipeline begin

--- file1.py
+++ file1.py
@@ -66,3 +66,30 @@ y_test  = y.iloc[outer_test_idx]
 print(X_train.shape, X_test.shape)
 print(y_train.value_counts(), "\n", y_test.value_counts())
 
+#einde splitten
+# korte versie
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold, GridSearchCV, cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import RobustScaler
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.linear_model import LogisticRegression
+
+# Data
+df = pd.read_csv("worclipo/Lipo_radiomicFeatures.csv")
+y = df["label"].map({"lipoma": 0, "liposarcoma": 1})
+X = df.drop(columns=["ID", "label"])
+
+# Nested CV
+outer_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+inner_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=1)
+
+# Pipeline
+pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy='median')), # Impute missing values with median
+    ('scaler', RobustScaler()), # Scale features using RobustScaler
+    ('feature_selection', SelectKBest(score_func=f_classif)), # Select top k features
+    ('classifier', LogisticRegression(max_iter=1000, random_state=42)) # Logistic Regression classifier
+])
+
