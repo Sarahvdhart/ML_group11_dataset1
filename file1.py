b8b1ffe3bf7f669f93d122a4dee8c65dd58f8976
laraverhoef123 <l.verhoef-1@student.tudelft.nl>
2026-03-03 15:55:54 +0100

    comments toegevoegd bij de pipeline

--- file1.py
+++ file1.py
@@ -87,9 +87,9 @@ inner_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=1)
 
 # Pipeline
 pipeline = Pipeline([
-    ('imputer', SimpleImputer(strategy='median')), # Impute missing values with median
+    ('imputer', SimpleImputer(strategy='median')), # Impute missing values with median, hier kijken of we bijvoorbeeld nog een threshold willen toevoegen voor als er bijvoorbeeld zoveel nullen zijn dat we die feature misschien willen verwijderen
     ('scaler', RobustScaler()), # Scale features using RobustScaler
-    ('feature_selection', SelectKBest(score_func=f_classif)), # Select top k features
-    ('classifier', LogisticRegression(max_iter=1000, random_state=42)) # Logistic Regression classifier
+    ('feature_selection', SelectKBest(score_func=f_classif)), # Select top k features --> ook hier kijken welke feature selection we nog moeten nemen!!!!
+    ('classifier', LogisticRegression(max_iter=1000, random_state=42)) # Logistic Regression classifier --> kijken welke classifier we nog moeten nemen!!!!
 ])
 
