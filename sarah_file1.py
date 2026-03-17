cfd19cf0ddf550999e6832e784546f36b5f86d1f
Sarahvdhart <S.L.vanderHart@student.tudelft.nl>
2026-03-17 16:32:17 +0100

    file1 extra

--- sarah_file1.py
+++ sarah_file1.py
@@ -13,7 +13,7 @@ from preprocessing import CustomPreprocessor
 from sklearn.metrics import roc_auc_score #toegevoegd
 from SVM import get_svm_pipeline, get_svm_param_grid
 #from RF import get_rf_pipeline, get_rf_param_grid
-#from XGB import get_xgb_pipeline, get_xgb_param_grid
+#from xgb import get_xgb_pipeline, get_xgb_param_grid
 
 
 # Load data
@@ -29,7 +29,7 @@ inner_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=1)
 models = {
     "SVM": (get_svm_pipeline(), get_svm_param_grid()), #nog definieren in svm.py
 #     "Random Forest": (get_rf_pipeline(), get_rf_param_grid()), #nog definieren in rf.py
- #    "XGBoost": (get_xgb_pipeline(), get_xgb_param_grid()) #nog definieren in xgb.py
+   #  "XGBoost": (get_xgb_pipeline(), get_xgb_param_grid()) #nog definieren in xgb.py
 } 
 
 # # for each model, perform nested cross-validation and print results
