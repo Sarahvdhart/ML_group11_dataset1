3c1e154ba24829d78197a43e0a89294df3ef6634
WoutervanWijck <woutervanwijck@ziggo.nl>
2026-03-17 12:26:17 +0100

    random forest zonder pipeline

--- Wouter.py
+++ Wouter.py
@@ -3,49 +3,70 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sklearn as sk
 from sklearn.preprocessing import RobustScaler
+from sklearn.model_selection import train_test_split
+from worclipo.load_data import load_data 
+from sklearn.pipeline import Pipeline
+from preprocessing import CustomPreprocessor
 
-from worclipo.load_data import load_data
+
+def get_rf_pipeline():
+    return Pipeline([
+        ("preprocess", CustomPreprocessor(
+            zero_threshold=0.90,
+            clip_iqr=False,
+            corr_threshold=0.85
+        )),
+        ("classifier", RandomForestClassifier(random_state=42))
+    ])
 
 
-df= load_data()
-
-# 1. Splits de dataframe in de eerste 3 kolommen en de rest
+df = load_data()
 df_to_keep = df.iloc[:, :3]
 df_to_scale = df.iloc[:, 3:]
 
-# 2. Initialiseer en fit de scaler
 scaler = RobustScaler()
-scaled_array = scaler.fit_transform(df_to_scale)
-
-# 3. Maak van de geschaalde array weer een DataFrame
-# We geven de kolomnamen en de index weer mee om alles netjes te houden
 df_scaled = pd.DataFrame(
-    scaled_array, 
+    scaler.fit_transform(df_to_scale), 
     columns=df_to_scale.columns, 
     index=df_to_scale.index)
 
+df = pd.concat([df_to_keep, df_scaled], axis=1)
 
-#Create the total dataframe with a robust scaler applicated
-df_final = pd.concat([df_to_keep, df_scaled], axis=1)
+# Splitting the data into features and target variable
+X = df.drop('label', axis=1)  # Alle kolommen behalve de target
+y = df['label']               # Alleen de kolom die je wilt voorspellen
 
-#Now we check for outliers in the scaled dataframe. We consider values < -5 or > 5 as extreme outliers.
-outlier_masker = (df_scaled < -3) | (df_scaled > 3)
+# Splitting the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-#Count the amount of outliers per column
-outliers_per_kolom = outlier_masker.sum()
 
-# Filter the columns that have more than 0 outliers and sort them in ascending order
-resultaat = outliers_per_kolom[outliers_per_kolom > 0].sort_values(ascending=True)
 
-# Print the results
-print("Aantal extreme uitschieters per kolom (> 3 of < -3):")
-print(resultaat)
+param_grid = {
+    'n_estimators': [10, 50, 100, 200],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'max_features': ['sqrt', 'log2']
+}
+# Initialise
+classifier = sk.ensemble.RandomForestClassifier(random_state=42)
 
-#Print the amount of columns that has more than 0 outliers
-print(f"The amount of columns with outliers: {resultaat.shape[0]}")
-#Amount of columns that has more than x% outliers
-total_rows = df_scaled.shape[0]
-percentage = 0.1
-columns_with_outliers = (resultaat > (percentage * total_rows)).sum()
-print(f"\nAantal kolommen met meer dan {percentage*100}% uitschieters: {columns_with_outliers}")
+random_search = sk.model_selection.RandomizedSearchCV(estimator= classifier,
+                                                       param_distributions = param_grid, 
+                                                         n_iter=20, 
+                                                         random_state= 42, 
+                                                         cv=5,
+)
+random_search.fit(X_train, y_train)
+
+# 1. Zet alle resultaten om in een overzichtelijke tabel
+results_df = pd.DataFrame(random_search.cv_results_)
+
+# 2. Sorteer op de hoogste score zodat de beste bovenaan staan
+results_df = results_df.sort_values(by='rank_test_score')
+
+# 3. Laat alleen de belangrijkste kolommen zien (parameters en score)
+# We filteren de kolommen die beginnen met 'param_' en de 'mean_test_score'
+important_cols = [col for col in results_df.columns if col.startswith('param_')] + ['mean_test_score']
+print(results_df[important_cols].head(10))
+
 
