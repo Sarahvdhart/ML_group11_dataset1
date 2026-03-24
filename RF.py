349439c26ed3162f321fb8963daef458489d01ec
WoutervanWijck <woutervanwijck@ziggo.nl>
2026-03-24 15:40:13 +0100

    again

--- RF.py
+++ RF.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt 
 import pandas as pd
+import numpy as np
 import sklearn as sk
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import RobustScaler
