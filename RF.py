bc46a5362309fbbfd6e3d40ebdca63b2886b0a75
WoutervanWijck <woutervanwijck@ziggo.nl>
2026-03-26 10:21:04 +0100

    Cleaned up the code

--- RF.py
+++ RF.py
@@ -22,19 +22,6 @@ def get_rf_pipeline():
     ])
 #------------------------------------------------------------------------------------------------
 # Defining the hyperparameter grid for Random Forest Classifier
-# def get_rf_param_grid():
-#     return {
-#         'classifier__n_estimators': [100, 500], #amount of trees
-#         'classifier__max_depth': [3,6], #aantal splitsingen
-#         'classifier__min_samples_split': [5],#minimum amount of samples for a split in a tree
-#         'classifier__min_samples_leaf': [4, 8], #minimum amount of samples for a leaf in a tree
-#         'classifier__max_features': ['sqrt', 'log2', 0.3], # 0.1 t/m 0.3 per decision, use only a subset of features for each tree
-#         'classifier__criterion': ['gini', 'log_loss'], #splitting criterion
-#         'classifier__max_samples': [None, 0.7, 0.8], #dont use all samples for each tree, use a subset of samples for each tree: bootstrap sampling
-#         'classifier__ccp_alpha': [0.0, 0.02] #how aggresive do you want to prune the tree (i.e. remove branches that have little importance)
-#         }
-#------------------------------------------------------------------------------------------------
-# Defining the hyperparameter grid for Random Forest Classifier
 def get_rf_param_grid():
     return {
         'classifier__n_estimators': [100, 200, 300, 400], #amount of trees
