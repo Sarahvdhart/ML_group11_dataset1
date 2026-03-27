03986f561514b62bd0e6b4c7d64568071d2c54ff
WoutervanWijck <woutervanwijck@ziggo.nl>
2026-03-27 14:14:15 +0100

    param grid

--- RF.py
+++ RF.py
@@ -21,13 +21,13 @@ def get_rf_pipeline():
 #Hyperparameter grid RF
 def get_rf_param_grid():
     return {
-        'classifier__n_estimators': [100, 200, 300, 400],                   
+        'classifier__n_estimators': list(range(100, 401, 5)),                   
         'classifier__max_depth': [3, 4, 5, 6], 
         'classifier__min_samples_split': list(range(5, 16)), 
-        'classifier__min_samples_leaf': [3, 5, 8], 
+        'classifier__min_samples_leaf': [3, 4, 5, 6, 7, 8], 
         'classifier__max_features': ['sqrt', 'log2', 0.2, 0.3], 
         'classifier__criterion': ['gini', 'log_loss'], 
         'classifier__max_samples': [None, 0.7, 0.8], 
-        'classifier__ccp_alpha': [0.0, 0.01, 0.02], 
+        'classifier__ccp_alpha': np.arange(0, 0.022, 0.002).tolist(), 
         'classifier__bootstrap': [True]
         }
